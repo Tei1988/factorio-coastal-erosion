a4e7c1162ca9a0dde9dd6daf038a47d9445d2d72
Tei1988 <tei1988@grifola.xyz>
2019-12-15 20:14:29 +0900

    Some settings are configureable

---
+++ src/settings.ts
@@ -0,0 +1,30 @@
+data.extend([
+    {
+        type: 'double-setting',
+        name: 'coastal-erosion-erosion-tiles-rate',
+        setting_type: 'startup',
+        minimum_value: 0,
+        maximum_value: 1,
+        default_value: 0.05,
+    },
+    {
+        type: 'int-setting',
+        name: 'coastal-erosion-max-erosion-tiles',
+        setting_type: 'startup',
+        minimum_value: 1,
+        default_value: 100,
+    },
+    {
+        type: 'int-setting',
+        name: 'coastal-erosion-erosion-speed',
+        setting_type: 'startup',
+        minimum_value: 10,
+        default_value: 1000,
+    },
+    {
+        type: 'bool-setting',
+        name: 'coastal-erosion-debug-output',
+        setting_type: 'runtime-per-user',
+        default_value: false,
+    },
+])
